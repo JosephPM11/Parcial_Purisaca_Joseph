e80f4f414dee1338c34fc8473c7926af67c4655c
Joseph Purisaca <josephpurisaca11@gmail.com>
2024-10-07 10:59:02 -0500

    Creación de carpeta

---
+++ src/Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente{
+    private String RUC;
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String clave;
+
+    public boolean agregarOferta(Oferta oferta){
+
+    }
+
+    public boolean eliminarOferta(Oferta oferta){
+
+    }
+
+    public Oferta[] getOfertas{
+
+    }
+}
